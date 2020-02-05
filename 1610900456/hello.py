51914d23cb22e39a8af8ce80ed32c1bf7bb12e89
tong200342 <57955847+tong200342@users.noreply.github.com>
2020-02-05 14:29:34 +0700

    two commit

---
+++ 1610900456/hello.py
@@ -0,0 +1,4 @@
+msg = "My neme is Tong"
+msg2 = "I'm from Thailand"
+print(msg+msg2)
+
---
+++ 1610900456/quiz.py
@@ -0,0 +1,4 @@
+msg = "My neme is Dararat"
+msg2 = "I'm from Thailand"
+msg3 = "I love catttttttt"
+print(msg+msg2+msg3)
